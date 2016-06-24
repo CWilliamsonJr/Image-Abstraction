1e06bc0dbef41147081a90b54b80c1a638c2dbb7
leo81202 <nucklesix@gmail.com>
2016-06-24 19:22:47 -0400

    added logic for recent searches

--- README.md
+++ README.md
@@ -6,23 +6,25 @@
 >3.I can get a list of the most recently submitted search strings.  
 
 Example Usage:  
-`www.cewjr.us/ImageSearch/imgsearch.php/[img you want to search]?offset=[number of returned results]`  
-`www.cewjr.us/ImageSearch/imgsearch.php/cars?offset=10`
+`[yoursite]/ImageSearch/imgsearch.php/[img you want to search]?offset=[number of returned results]`  
+`[yoursite]/ImageSearch/imgsearch.php/cars?offset=10`
 
 **NOTE:Max results possible is 50.**
 
 Example Output:
 ```json
 {  
-Title: "Cars News and Images: Cars pictures",  
-Source: "http://cars-news-images.blogspot.com/p/cars-pictures_26.html",  
-ImgUrl:"http://2.bp.blogspot.com/-DXumDyjcLf4/USyuoPUXYRI/AAAAAAAAASw/-TNzVUSDAcE/s1600/American-cars-Camaro-convertible_car.jpg",  
-Thumbnail: "http://ts4.mm.bing.net/th?id=OIP.M58c43935d427addd462e40723594360cH0&pid=15.1"  
+  Title: "Ferrari Enzo Wallpaper HD | Cars WallPaper HD",
+  Source: "http://carswallpaperhd.com/ferrari-enzo-wallpaper-hd.html",
+  ImgUrl: "http://carswallpaperhd.com/wp-content/uploads/2012/06/ferrari-enzo.jpg",
+  Thumbnail: "http://ts1.mm.bing.net/th?id=OIP.M1168b2fdb13210999cc1eb1f3b36690aH0&pid=15.1"  
 },  
 {  
-Title: "home vehicles cars cars photo tags photo muscle car",  
-Source: "http://myphotos.eu/photography-69_cars_photo_muscle_car.html",  
-ImgUrl: "http://www.myphotos.eu/photos/vehicles/cars/69_cars_photo_muscle_car.jpg",  
-Thumbnail: "http://ts2.mm.bing.net/th?id=OIP.Mc082e2d4a54529995c2aa0dd50eb98ecH0&pid=15.1"  
+  Title: "home vehicles cars cars photo tags photo muscle car",  
+  Source: "http://myphotos.eu/photography-69_cars_photo_muscle_car.html",  
+  ImgUrl: "http://www.myphotos.eu/photos/vehicles/cars/69_cars_photo_muscle_car.jpg",  
+  Thumbnail: "http://ts2.mm.bing.net/th?id=OIP.Mc082e2d4a54529995c2aa0dd50eb98ecH0&pid=15.1"  
 },
 ```
+For recent searches:  
+`[yoursite]/ImageSearch/imgsearch.php/recentsearches`
--- imgsearch.php
+++ imgsearch.php
@@ -6,59 +6,26 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+    <?php
+      require './includes/functions.inc';
+     ?>
   </head>
   <body>
     <?php
-//TODO: Add a way to store previous search results.
 
     if(empty(basename($_SERVER['PATH_INFO']))){ // if nothing is being searched show a webpage
-      $site =  $_SERVER['SERVER_NAME']; //website path
-      $html = <<<HTML
-<div class='container'>
-  <div class='row'>
-    <div class='h1'>Image Search Abstraction Layer <span class='h4'>Completed with PHP</span> </div>
-    <blockquote>
-    User Stories:
-    <ol>
-    <li>I can get the image URLs, alt text and page urls for a set of images relating to a given search string. </li>
-    <li>I can paginate through the responses by adding a ?offset=2 parameter to the URL. </li>
-    <li>I can get a list of the most recently submitted search strings. </li>
-    </ol>
-    </blockquote>
-    </div>
-    <div class='h3'>Example Usage: </div>
-    <div><code>$site/ImageSearch/imgsearch.php/[img you want to search]?offset=[number of returned results]</code></div>
-    <div><code>$site/ImageSearch/imgsearch.php/cars?offset=10</code></div> <br/>
-    <div> </div>
-    <div class='h3'>Example Output: </div>
-    <code>
-          { <br/>
-      Title: "Cars News and Images: Cars pictures", <br/>
-      Source: "http://cars-news-images.blogspot.com/p/cars-pictures_26.html", <br/>
-      ImgUrl: "http://2.bp.blogspot.com/-DXumDyjcLf4/USyuoPUXYRI/AAAAAAAAASw/-TNzVUSDAcE/s1600/American-cars-Camaro-convertible_car.jpg", <br/>
-      Thumbnail: "http://ts4.mm.bing.net/th?id=OIP.M58c43935d427addd462e40723594360cH0&pid=15.1" <br/>
-      }, <br/>
-      { <br/>
-      Title: "home vehicles cars cars photo tags photo muscle car", <br/>
-      Source: "http://myphotos.eu/photography-69_cars_photo_muscle_car.html", <br/>
-      ImgUrl: "http://www.myphotos.eu/photos/vehicles/cars/69_cars_photo_muscle_car.jpg", <br/>
-      Thumbnail: "http://ts2.mm.bing.net/th?id=OIP.Mc082e2d4a54529995c2aa0dd50eb98ecH0&pid=15.1" <br/>
-      },
-    </code>
-  </div>
-</div>
-HTML;
-      echo $html;
-    }else{
+        require './includes/launchpage.inc';
+    }else if((basename($_SERVER['PATH_INFO'])) === 'recentsearches'){ // show recent searches page
+        require './includes/recentsearch.inc';
+    } else{
+
+      $filename = 'searches.txt';
       define('MAX_RESULTS', 50); // Max possible results set to a constant
       $search = urlencode(basename($_SERVER['PATH_INFO'])); // gets the serch term from the URL
       $url = 'https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query=%27'.$search.'%27'; // sets the url for the search
       $accountKey = '62yWlZ8o2DS8p1uBXEbx/J1rBHoGSLFY3JNVstd1qDE'; // API key for bing search
 
       $query = $_SERVER['QUERY_STRING']; // gets the offset if there's one
-
-      //echo "$search <br/>";
 
       // Initiate curl
       $ch = curl_init();
@@ -78,8 +45,8 @@ HTML;
       $response = curl_exec($ch);
       // closes curl
       curl_close($ch);
-      $json = json_decode($response,true); // decodes the response to an array
 
+      $json = json_decode($response,true); // decodes the response to an array
       parse_str($query); // gets offset from the users
 
       if(isset($offset)){ // detects if offset was in the query string
@@ -90,7 +57,7 @@ HTML;
         $offset = count($json['d']['results']); // sets default offset to total amount returned
       }
 
-      for($i = 0; $i < $offset; $i++){
+      for($i = 0; $i < $offset; $i++){ // sorts through the retured request and makes a new array with the wanted values.
         $value = $json['d']['results'][$i];
         $array[] = array(
           'Title' => $value['Title'],
@@ -99,6 +66,19 @@ HTML;
           'Thumbnail' => $value['Thumbnail']['MediaUrl']
             );
       }
+      $filecontents = [ // timestamp for what was seached and time of search to be added to a file.
+        'term' => $search,
+        'when' => date('M-d-Y g:i a T',$_SERVER['REQUEST_TIME'])
+      ];
+
+      if(is_readable($filename)){ // detects if file is readiable.
+          AppendPastSearches($filecontents);
+        }else{
+          $handle = fopen($filename, "a"); // if file doesn't exist create the file;
+          fclose($handle);
+          AppendPastSearches($filecontents);
+        }
+
       if(!empty($array)){
         echo json_encode($array,JSON_PRETTY_PRINT);
       }
